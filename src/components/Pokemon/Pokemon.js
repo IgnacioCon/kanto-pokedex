895d6e6326ac1f93baf603e9426aaa79f6b09689
IgnacioCon <contrerasign@gmail.com>
2021-10-20 16:43:58 -0500

    now fetches from api and renders information correctly

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -5,19 +5,17 @@ import './PokemonCard.css';
 const Pokemon = (props) => {
   const { sprites, ...details } = props.pokemon;
   const { types, height, weight, ...idName } = details;
-  const typeArray = Array.from(Object.values(types[0]));
-
   return (
     <div
       className='card'
       style={{
-        backgroundImage: `url(types/${typeArray[0].name}.png)`,
+        backgroundImage: `url(types/${types[0].type.name}.png)`,
         backgroundSize: 'cover',
       }}
     >
       <PokemonImage image={sprites} name={details.name} />
       <PokemonDetail
-        types={typeArray}
+        types={types}
         height={height}
         weight={weight}
         idName={idName}
--- src/components/Pokemon/PokemonImage.js
+++ src/components/Pokemon/PokemonImage.js
@@ -2,7 +2,7 @@ import './PokemonCard.css';
 
 const PokemonImage = ({ image, name }) => {
   const { other } = image;
-  const pkmnImg = Object.entries(other)[0][1];
+  const pkmnImg = Object.entries(other)[1][1];
 
   return <img className='pokemon-img' src={pkmnImg.front_default} alt={name} />;
 };
